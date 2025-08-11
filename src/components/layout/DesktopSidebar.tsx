4796882b87f6f5a71aaa6781630a1bde705a0440
Padel metodo3 <padelmetodo3@gmail.com>
2025-08-11 18:10:55 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/components/layout/DesktopSidebar.tsx
+++ src/components/layout/DesktopSidebar.tsx
@@ -2,7 +2,7 @@
 
 "use client";
 
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useMemo } from 'react';
 import Link from 'next/link';
 import Image from 'next/image';
 import { cn, getInitials } from '@/lib/utils';
